2bfaa479da4c25cabe921d1aff867ea937d3e320
Smatsumot0 <killer0xxxx@gmail.com>
2026-04-26 23:01:44 +0900

    chore: remove duplicated api root files

--- src/auth/user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { AuthUser } from './jwt.strategy';
-
-export const CurrentUser = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext): AuthUser => {
-    const req = ctx.switchToHttp().getRequest();
-    return req.user;
-  },
-);
